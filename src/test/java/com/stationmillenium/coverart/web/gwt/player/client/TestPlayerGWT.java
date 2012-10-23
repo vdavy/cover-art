41bdabbd6ec116738fa715872d3bc46466262028
Vincent <vdavy.mobile@gmail.com>
2012-10-23 19:37:18 +0200

    Add GWT unit tests

---
+++ src/test/java/com/stationmillenium/coverart/web/gwt/player/client/TestPlayerGWT.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package com.stationmillenium.coverart.web.gwt.player.client;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.junit.client.GWTTestCase;
+import com.stationmillenium.coverart.web.gwt.player.client.activities.PlayerActivity;
+import com.stationmillenium.coverart.web.gwt.player.client.clientfactory.ClientFactory;
+
+/**
+ * Test case for player
+ * @author vincent
+ *
+ */
+public class TestPlayerGWT extends GWTTestCase {
+
+	/* (non-Javadoc)
+	 * @see com.google.gwt.junit.client.GWTTestCase#getModuleName()
+	 */
+	@Override
+	public String getModuleName() {
+		return "com.stationmillenium.coverart.web.gwt.player.Player";
+	}
+
+	/**
+	 * Test player update
+	 */
+	public void testUpdatePlayer() {
+		ClientFactory clientFactory = GWT.create(ClientFactory.class);
+		new PlayerActivity(clientFactory).updatePlayer();
+	}
+	
+	/**
+	 * Test player history update
+	 */
+	public void testUpdateHistoryList() {
+		ClientFactory clientFactory = GWT.create(ClientFactory.class);
+		PlayerActivity playerActivity = new PlayerActivity(clientFactory);
+		playerActivity.updateHistoryList(true);
+		playerActivity.updateHistoryList(false);
+	}
+	
+	/**
+	 * Test player update
+	 */
+	public void testStreamURLs() {
+		ClientFactory clientFactory = GWT.create(ClientFactory.class);
+		String[] urls = new PlayerActivity(clientFactory).getStreamURLs();
+		assertTrue(urls.length != 0);
+	}
+}
