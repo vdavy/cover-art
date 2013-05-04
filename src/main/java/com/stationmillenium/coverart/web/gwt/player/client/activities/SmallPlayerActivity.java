127da66ef9114ef09ecea025b8fdd80d7ebf0e35
Vincent <vdavy.mobile@gmail.com>
2013-05-04 11:16:12 +0200

    -adjustments

--- src/main/java/com/stationmillenium/coverart/web/gwt/player/client/activities/SmallPlayerActivity.java
+++ src/main/java/com/stationmillenium/coverart/web/gwt/player/client/activities/SmallPlayerActivity.java
@@ -7,6 +7,8 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import com.google.gwt.activity.shared.AbstractActivity;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.RunAsyncCallback;
 import com.google.gwt.core.client.ScriptInjector;
 import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.user.client.rpc.AsyncCallback;
@@ -53,38 +55,49 @@ public class SmallPlayerActivity extends AbstractActivity implements SmallPlayer
 
 	@Override
 	public void updateSmallPlayer() {
-		//load current song
-		clientFactory.getService().getLastSong(new AsyncCallback<SongGWTDTO>() {
-
+		GWT.runAsync(new RunAsyncCallback() {
+			
 			@Override
-			public void onSuccess(SongGWTDTO newSong) {
-				if ((newSong != null) && (newSong.getArtist() != null) && (newSong.getTitle() != null)) { //if some song provided
-					if (!newSong.equals(currentSong)) {
-						//set up image and text
-						String text = clientFactory.getMessages().currentSongText(newSong.getArtist(), newSong.getTitle());
-						clientFactory.getSmallPlayer().setCurrentSong(text);
-												
-						//finish update						
-						currentSong = newSong;
-						
-						LOGGER.info("Small  player updated : " + newSong);
-						
-					} else
-						LOGGER.fine("Small player already up-to-date");					
-				} else { 
-					LOGGER.warning("Small player updated : null");
-					displayErrorSong();
-				}			
-				
-			}
+			public void onSuccess() {
+				//load current song
+				clientFactory.getService().getLastSong(new AsyncCallback<SongGWTDTO>() {
 
-			@Override
-			public void onFailure(Throwable caught) { //in case of error
-				LOGGER.log(Level.SEVERE, "Error during small player update", caught);
-				displayErrorSong();
+					@Override
+					public void onSuccess(SongGWTDTO newSong) {
+						if ((newSong != null) && (newSong.getArtist() != null) && (newSong.getTitle() != null)) { //if some song provided
+							if (!newSong.equals(currentSong)) {
+								//set up image and text
+								String text = clientFactory.getMessages().currentSongText(newSong.getArtist(), newSong.getTitle());
+								clientFactory.getSmallPlayer().setCurrentSong(text);
+														
+								//finish update						
+								currentSong = newSong;
+								
+								LOGGER.info("Small  player updated : " + newSong);
+								
+							} else
+								LOGGER.fine("Small player already up-to-date");					
+						} else { 
+							LOGGER.warning("Small player updated : null");
+							displayErrorSong();
+						}			
+						
+					}
+
+					@Override
+					public void onFailure(Throwable caught) { //in case of error
+						LOGGER.log(Level.SEVERE, "Error during small player update", caught);
+						displayErrorSong();
+					}
+					
+				});				
 			}
 			
-		});	
+			@Override
+			public void onFailure(Throwable reason) {
+				LOGGER.log(Level.SEVERE, "Error while loading small player", reason);
+			}
+		});
 	}
 
 
