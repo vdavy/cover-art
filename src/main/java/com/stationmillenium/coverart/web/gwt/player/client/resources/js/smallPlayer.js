08e3b8ab6b170173fc66a975ae0c2f7a2e12c81b
Vincent DAVY <vdavy.mobile@gmail.com>
2015-03-30 21:10:42 +0200

    fix player windows size

--- src/main/java/com/stationmillenium/coverart/web/gwt/player/client/resources/js/smallPlayer.js
+++ src/main/java/com/stationmillenium/coverart/web/gwt/player/client/resources/js/smallPlayer.js
@@ -1,4 +1,4 @@
 //declare function to send tracking to piwik
 $wnd.openMainPlayer = function(id) {
-	$wnd.open('http://www.station-millenium.com/coverart/player.html','popup','width=530,height=500,top=20,left=20,toolbar=0,location=0,directories=0,status=0,menubar=0,scrollbars=auto,resizable=0');
+	$wnd.open('http://www.station-millenium.com/coverart/player.html','popup','width=540,height=500,top=20,left=20,toolbar=0,location=0,directories=0,status=0,menubar=0,scrollbars=auto,resizable=0');
 }
